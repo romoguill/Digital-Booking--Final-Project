0d460cf30d10a178db8cd3c81ad8128f756c1dac
romoguill <romoguill@gmail.com>
2023-02-15 14:58:57 -0300

    Set up basic structure for Home page and Header, Body and Footer components

---
+++ frontEnd/src/Routes/Home.jsx
@@ -0,0 +1,15 @@
+import Header from '../Components/Header';
+import Body from '../Components/Body';
+import Footer from '../Components/Footer';
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
