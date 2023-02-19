a4177d67b351d3fe287f653976a5f211289bd138
romoguill <romoguill@gmail.com>
2023-02-18 21:37:14 -0300

    Finished styling the footer of the Menu Drawer

--- frontEnd/src/Components/MenuDrawerMobile/MenuDrawerMobile.jsx
+++ frontEnd/src/Components/MenuDrawerMobile/MenuDrawerMobile.jsx
@@ -3,36 +3,48 @@ import instagramLogo from '../../assets/socialMedia/instagram.png';
 import linkedinLogo from '../../assets/socialMedia/linkedin.png';
 import twitterLogo from '../../assets/socialMedia/twitter.png';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
+
 import './MenuDrawerMobile.scss';
 
 function MenuDrawerMobile() {
   return (
     <aside className="menu-drawer-mobile">
-      <div className="menu-drawer-mobile__container">
-        <div className="menu-drawer-mobile__header">
+      <button className="close-menu button-ghost">
+        <FontAwesomeIcon icon={faXmark} />
+      </button>
+      <div className="menu-drawer-mobile__header">
+        <div className="menu-drawer-mobile__container">
           <h2>MENÚ</h2>
         </div>
-        <div className="menu-drawer-mobile__body">
+      </div>
+      <div className="menu-drawer-mobile__body">
+        <div className="menu-drawer-mobile__container">
           <h3>Crear cuenta</h3>
           <h3>Iniciar Sesion</h3>
         </div>
-        <div className="menu-drawer-mobile__footer">
-          <div className="footer__copyright">
-            <h3>Logo</h3>
-            <p>© 2023 SPRINGSENSE</p>
-          </div>
+      </div>
+      <div className="menu-drawer-mobile__footer">
+        <div className="menu-drawer-mobile__container">
           <div className="container__social-media">
             <a href="#">
-              <img src={facebookLogo} />
+              <FontAwesomeIcon icon={faFacebook} />
             </a>
             <a href="#">
-              <img src={instagramLogo} />
+              <FontAwesomeIcon icon={faInstagram} />
             </a>
             <a href="#">
-              <img src={linkedinLogo} />
+              <FontAwesomeIcon icon={faLinkedin} />
             </a>
             <a href="#">
-              <img src={twitterLogo} />
+              <FontAwesomeIcon icon={faTwitter} />
             </a>
           </div>
         </div>
