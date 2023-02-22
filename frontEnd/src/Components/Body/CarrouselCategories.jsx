c91438797a92c8167a72789555760765c98d0f9c
romoguill <romoguill@gmail.com>
2023-02-22 09:49:09 -0300

    Started fixing the responsiveness of Category Grid

--- frontEnd/src/Components/Body/CarrouselCategories.jsx
+++ frontEnd/src/Components/Body/CarrouselCategories.jsx
@@ -1,39 +1,85 @@
-import { useEffect, useState } from "react";
-import "./CarrouselCategories.scss";
-import CategoriaCard from "./CategoriaCard";
+import { useEffect, useState } from 'react';
+import './CarrouselCategories.scss';
+import CategoriaCard from './CategoriaCard';
 
 function CarrouselCategories() {
   const [categorias, setCategorias] = useState([]);
   const [error, setError] = useState();
 
   useEffect(() => {
-    const options = { method: "GET" };
+    const options = { method: 'GET' };
 
-    fetch("http://localhost:8080/categoria/todas", options)
+    fetch('http://localhost:8080/categoria/todas', options)
       .then((response) => {
         if (!response.ok) throw new Error();
         return response.json();
       })
       .then((response) => setCategorias(response))
       .catch((e) => {
-        setError("No se pudo recuperar la informacion");
+        setError('No se pudo recuperar la informacion');
         console.error(e);
       });
   }, []);
 
+  // TODO: Eliminar el codigo relevante a las pruebas estaticas
+
+  const dataPrueba = [
+    {
+      id: 1,
+      data: {
+        titulo: 'Hoteles',
+        urlImagen: 'src/assets/Images/hotel-img-1.png',
+      },
+    },
+
+    {
+      id: 2,
+      data: {
+        titulo: 'Hostels',
+        urlImagen: 'src/assets/Images/hotel-img-2.png',
+      },
+    },
+
+    {
+      id: 3,
+      data: {
+        titulo: 'Departamentos',
+        urlImagen: 'src/assets/Images/hotel-img-3.png',
+      },
+    },
+
+    {
+      id: 4,
+      data: {
+        titulo: 'Bed and breakfast',
+        urlImagen: 'src/assets/Images/hotel-img-4.jpg',
+      },
+    },
+  ];
+
   return (
     <section className="section__categories">
-      <h2>Buscar por tipo de alojamiento</h2>
-      <div className="container-categories">
-        {!error ? (
-          categorias
-            .slice(0, 4)
-            .map((categoria) => (
-              <CategoriaCard key={categoria.id} data={categoria} />
-            ))
-        ) : (
-          <p>{error}</p>
-        )}
+      <div className="container-main">
+        <h2 className="text-dark section-header">
+          Buscar por tipo de alojamiento
+        </h2>
+        <div className="container-categories">
+          <CategoriaCard key={dataPrueba[0].id} data={dataPrueba[0].data} />
+          <CategoriaCard key={dataPrueba[1].id} data={dataPrueba[1].data} />
+          <CategoriaCard key={dataPrueba[2].id} data={dataPrueba[2].data} />
+          <CategoriaCard key={dataPrueba[3].id} data={dataPrueba[3].data} />
+
+          {/* Descomentar al terminar las pruebas */}
+          {/* {!error ? (
+            categorias
+              .slice(0, 4)
+              .map((categoria) => (
+                <CategoriaCard key={categoria.id} data={categoria} />
+              ))
+          ) : (
+            <p className="text-dark">{error}</p>
+          )} */}
+        </div>
       </div>
     </section>
   );
