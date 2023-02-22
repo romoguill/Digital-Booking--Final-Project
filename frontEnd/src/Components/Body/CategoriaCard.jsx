2a95baf35b5dfd27dcd1ce41035d74d31645416b
romoguill <romoguill@gmail.com>
2023-02-22 13:07:21 -0300

    Carrousel responsive finished

--- frontEnd/src/Components/Body/CategoriaCard.jsx
+++ frontEnd/src/Components/Body/CategoriaCard.jsx
@@ -1,12 +1,16 @@
-import React from "react";
-import "./CarrouselCategories.scss";
+import React from 'react';
+import './CarrouselCategories.scss';
 
 const CategoriaCard = ({ data, id }) => {
   return (
     <div key={id} className="card-category">
-      <img src={data.urlImagen} alt="Imagen de la Categoria" />
-      <h3>{data.titulo}</h3>
-      <p>807.105 hoteles</p>
+      <div className="container-img">
+        <img src={data.urlImagen} alt="Imagen de la Categoria" />
+      </div>
+      <div className="container-content">
+        <h3>{data.titulo}</h3>
+        <p>807.105 hoteles</p>
+      </div>
     </div>
   );
 };
