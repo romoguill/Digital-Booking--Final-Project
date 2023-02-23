8d0bdd654c1ff99861acae7d1c7c77f5b088c336
romoguill <romoguill@gmail.com>
2023-02-23 00:08:33 -0300

    Set up Context for user auth information

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -4,42 +4,53 @@ import Home from './Routes/Home';
 import Login from './Routes/Login';
 import CreateUser from './Routes/CreateUser';
 
+import { UserContext } from './Contexts/Context';
+
 function App() {
   // Estado que determina si el menu lateral en mobile esta visible
   const [menuDrawerVisible, setMenuDrawerVisible] = useState(false);
+  const [userAuthInfo, setUserAuthInfo] = useState({
+    isLoggedIn: false,
+    userInfo: {
+      name: undefined,
+      lastName: undefined,
+    },
+  });
 
   return (
-    <div className="App">
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Home
-              menuDrawerVisible={menuDrawerVisible}
-              setMenuDrawerVisible={setMenuDrawerVisible}
-            />
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <Login
-              menuDrawerVisible={menuDrawerVisible}
-              setMenuDrawerVisible={setMenuDrawerVisible}
-            />
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            <CreateUser
-              menuDrawerVisible={menuDrawerVisible}
-              setMenuDrawerVisible={setMenuDrawerVisible}
-            />
-          }
-        />
-      </Routes>
-    </div>
+    <UserContext.Provider value={{ userAuthInfo, setUserAuthInfo }}>
+      <div className="App">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                menuDrawerVisible={menuDrawerVisible}
+                setMenuDrawerVisible={setMenuDrawerVisible}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <Login
+                menuDrawerVisible={menuDrawerVisible}
+                setMenuDrawerVisible={setMenuDrawerVisible}
+              />
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <CreateUser
+                menuDrawerVisible={menuDrawerVisible}
+                setMenuDrawerVisible={setMenuDrawerVisible}
+              />
+            }
+          />
+        </Routes>
+      </div>
+    </UserContext.Provider>
   );
 }
 
--- frontEnd/src/Routes/Home.jsx
+++ frontEnd/src/Routes/Home.jsx
@@ -4,10 +4,15 @@ import HomeSearch from '../Components/Body/HomeSearch';
 import CarrouselCategories from '../Components/Body/CarrouselCategories';
 import GridRentals from '../Components/Body/GridRentals';
 import MenuDrawerMobile from '../Components/MenuDrawerMobile/MenuDrawerMobile';
+import { useContext } from 'react';
+import { UserContext } from '../Contexts/Context';
 
 function Home({ menuDrawerVisible, setMenuDrawerVisible }) {
+  const { userAuthInfo, setUserAuthInfo } = useContext(UserContext);
+
   return (
     <div className="container-page">
+      {console.log(userAuthInfo)}
       {menuDrawerVisible && (
         <MenuDrawerMobile setMenuDrawerVisible={setMenuDrawerVisible} />
       )}
