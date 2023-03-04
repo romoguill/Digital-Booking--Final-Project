df8cd99a6ec6e6b956c6fe4d48a07f394384aa91
romoguill <romoguill@gmail.com>
2023-03-04 07:45:30 -0300

    Finished integrating with Open Map and Leaflet

--- frontEnd/src/Components/RentalProducts/Map.jsx
+++ frontEnd/src/Components/RentalProducts/Map.jsx
@@ -3,22 +3,21 @@ import 'leaflet/dist/leaflet.css';
 
 function Map() {
   const initalSettings = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627,
-    },
-    zoom: 11,
+    center: [-34.5486686823985, -58.44360312868901],
+    zoom: 15,
   };
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      center={initalSettings.center}
+      zoom={initalSettings.zoom}
+      scrollWheelZoom={false}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
+      <Marker position={[-34.5486686823985, -58.44360312868901]}>
+        <Popup></Popup>
       </Marker>
     </MapContainer>
   );
