a9f26b520e0b0f1d1d38c6cc54a7b08358e7d6ba
romoguill <romoguill@gmail.com>
2023-03-15 09:25:49 -0300

    Created route for /producto/:id/reserva. Set up basic html and style structure

---
+++ frontEnd/src/Components/BannerProductTitle.jsx
@@ -0,0 +1,24 @@
+import './BannerProductTitle.scss';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+
+import { Link } from 'react-router-dom';
+
+function BannerProductTitle({ titulo, categoria }) {
+  return (
+    <div className="hero__products__title">
+      <div className="container-main">
+        <div className="hero__products__title__detail">
+          <h2>{categoria}</h2>
+          <h1>{titulo}</h1>
+        </div>
+        <Link to="/">
+          <FontAwesomeIcon className="go-back-icon" icon={faChevronLeft} />
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default BannerProductTitle;
--- frontEnd/src/Routes/RentalProducts.jsx
+++ frontEnd/src/Routes/RentalProducts.jsx
@@ -1,12 +1,24 @@
 import './RentalProducts.scss';
 
-import { faClock, faWheelchair, faUtensils, faCar, faBanSmoking, faSwimmer, faShieldHalved, faJugDetergent, faSpa, faWifi, faBorderNone } from '@fortawesome/free-solid-svg-icons';
+import {
+  faClock,
+  faWheelchair,
+  faUtensils,
+  faCar,
+  faBanSmoking,
+  faSwimmer,
+  faShieldHalved,
+  faJugDetergent,
+  faSpa,
+  faWifi,
+  faBorderNone,
+} from '@fortawesome/free-solid-svg-icons';
 
 import GalleryProduct from '../Components/RentalProducts/GalleryProduct';
 import Hero from '../Components/RentalProducts/Hero';
 import Map from '../Components/RentalProducts/Map';
 import CustomCalendar from '../Components/RentalProducts/CustomCalendar';
-import { Navigate, useParams } from 'react-router-dom';
+import { Navigate, useParams, Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -33,17 +45,19 @@ function RentalProducts() {
 
   useEffect(() => {
     const fetchData = async () => {
-      axios(`http://localhost:8080/productos/id=${params.id}`).then((res) => {
-        setProducto(res.data.producto);
-        setImagenes(res.data.imagenes);
-      }).catch((res) => {
-        if(res.response.status == 404) {
-          setNoFound(true)
-        }
-      });
+      axios(`http://localhost:8080/productos/id=${params.id}`)
+        .then((res) => {
+          setProducto(res.data.producto);
+          setImagenes(res.data.imagenes);
+        })
+        .catch((res) => {
+          if (res.response.status == 404) {
+            setNoFound(true);
+          }
+        });
     };
     fetchData();
-  }, [])
+  }, []);
 
   return (
     <div className="rental-products container-page">
@@ -55,23 +69,29 @@ function RentalProducts() {
         <h2 className="product__description__title section-title">
           Alójate en el corazón de {producto.ciudad && producto.ciudad.nombre}
         </h2>
-        <p className="product__description__body">
-          {producto.descripcion}
-        </p>
+        <p className="product__description__body">{producto.descripcion}</p>
       </section>
 
       <section className="product__ammenities container-main">
         <h2 className="section-title">¿Qué ofrece este lugar?</h2>
         <hr className="section-divider" />
         <div className="product__ammenities__items">
-          {producto.caracteristicas && producto.caracteristicas.map((item, i) => {
-            return (
-              <div className="item" key={i}>
-                <FontAwesomeIcon icon={iconComponents[item.id]? iconComponents[item.id] : iconComponents[0] } className="ammenity-icon" />
-                <h4>{item.titulo}</h4>
-              </div>
-            );
-          })}
+          {producto.caracteristicas &&
+            producto.caracteristicas.map((item, i) => {
+              return (
+                <div className="item" key={i}>
+                  <FontAwesomeIcon
+                    icon={
+                      iconComponents[item.id]
+                        ? iconComponents[item.id]
+                        : iconComponents[0]
+                    }
+                    className="ammenity-icon"
+                  />
+                  <h4>{item.titulo}</h4>
+                </div>
+              );
+            })}
         </div>
       </section>
 
@@ -81,9 +101,12 @@ function RentalProducts() {
           <CustomCalendar producto={producto} imagenes={imagenes} />
           <div className="booking__call-to-action">
             <h4>Agregá tus fechas de viaje para obtener precios exactos</h4>
-            <button className="button-primary button-primary--full">
+            <Link
+              className="button-primary button-primary--full"
+              to={`/producto/${params.id}/reserva`}
+            >
               Iniciar reserva
-            </button>
+            </Link>
           </div>
         </div>
       </section>
@@ -91,7 +114,9 @@ function RentalProducts() {
       <section className="location container-main">
         <h2 className="section-title">¿Dónde vas a estar?</h2>
         <hr className="section-divider" />
-        <p className="location-address">{producto.ciudad && producto.ciudad.nombre}, Argentina</p>
+        <p className="location-address">
+          {producto.ciudad && producto.ciudad.nombre}, Argentina
+        </p>
         <Map producto={producto} imagenes={imagenes} />
       </section>
 
@@ -99,14 +124,18 @@ function RentalProducts() {
         <h2 className="section-title">¿Qué tenés que saber?</h2>
         <hr className="section-divider" />
         <div className="policy__items">
-          {producto.politicas && producto.politicas.map((item, i) => {
-            return (
-              <div className="item" key={i}>
-                <h4><img src={item.url} className="politica-icon" /> {item.titulo}</h4>
-                <p>{item.descripcion}</p>
-              </div>
-            );
-          })}
+          {producto.politicas &&
+            producto.politicas.map((item, i) => {
+              return (
+                <div className="item" key={i}>
+                  <h4>
+                    <img src={item.url} className="politica-icon" />{' '}
+                    {item.titulo}
+                  </h4>
+                  <p>{item.descripcion}</p>
+                </div>
+              );
+            })}
         </div>
       </section>
     </div>
