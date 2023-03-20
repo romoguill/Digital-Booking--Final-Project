48e9b247429f39d9c49b2866fcee3be3ad27558b
burgosfacundo <facundoandresbur@gmail.com>
2023-03-20 09:46:48 -0300

    Commit

--- backEnd/digitalBooking/src/main/java/com/example/digitalBooking/repository/CaracteristicaRepository.java
+++ backEnd/digitalBooking/src/main/java/com/example/digitalBooking/repository/CaracteristicaRepository.java
@@ -4,7 +4,6 @@ import com.example.digitalBooking.model.Caracteristica;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import java.util.Optional;
 
 public interface CaracteristicaRepository extends JpaRepository<Caracteristica,Long> {
