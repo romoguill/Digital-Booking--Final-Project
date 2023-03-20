f3ddc2e5d5c5ba22219f3460eaf7df5e112eb381
burgosfacundo <facundoandresbur@gmail.com>
2023-03-20 17:39:23 -0300

    Change extension jest.config

--- frontEnd/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    testEnvironment:"jest-environment-jsdom",
-    setupFilesAfterEnv:["<rootDir>/.jest/setup-test.js"],
-    moduleNameMapper:{
-        "\\.(gif|ttf|svg|jpg|jpeg)$":"<rootDir>/.jest/mocks/fileMock.js",
-        "\\.(css|less|sass|scss)$":"identity-obj-proxy"
-    }
-}
