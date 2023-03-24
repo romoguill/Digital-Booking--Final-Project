17750206e7bebefefb3c08c8e064b3c5f6a80232
romoguill <romoguill@gmail.com>
2023-03-24 18:45:06 -0300

    Fixed styles in booking success page

--- frontEnd/src/Routes/BookingSuccess.jsx
+++ frontEnd/src/Routes/BookingSuccess.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import './BookingSuccess.scss'
-import ReservaIcon from '../assets/Images/reserva-exito.png'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './BookingSuccess.scss';
+import ReservaIcon from '../assets/Images/reserva-exito.png';
+import { Link } from 'react-router-dom';
 
 const BookingSuccess = () => {
   return (
-    <div className='success-container'>
-        <div className='success-card'>
-            <img src={ReservaIcon} alt="Reserva Exitosa" />
-            <h2>¡Muchas gracias!</h2>
-            <p>Su reserva se ha realizado con éxito</p>
-            <Link to={'home'}>
-                <button>Ok</button>
-            </Link>
+    <div className="container-page">
+      <div className="success-container">
+        <div className="success-card">
+          <img src={ReservaIcon} alt="Reserva Exitosa" />
+          <h2>¡Muchas gracias!</h2>
+          <p>Su reserva se ha realizado con éxito</p>
+          <Link to={'home'}>
+            <button>Ok</button>
+          </Link>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BookingSuccess
+export default BookingSuccess;
