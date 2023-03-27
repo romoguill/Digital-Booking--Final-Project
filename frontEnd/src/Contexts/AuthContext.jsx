4870f50160a6551ac78f699979caa72fdbb9efb6
Rodrigo Moguillansky <romoguill@gmail.com>
2023-03-27 17:51:50 -0300

    Fixed units of measurement between JS Date and JSON date

--- frontEnd/src/Contexts/AuthContext.jsx
+++ frontEnd/src/Contexts/AuthContext.jsx
@@ -35,7 +35,7 @@ export const AuthContextProvider = ({ children }) => {
   };
 
   const tokenIsNotExpired = (tokenDecoded) => {
-    return tokenDecoded.exp < new Date();
+    return tokenDecoded.exp > Date.now() / 1000;
   };
 
   const isValidToken = (token) => {
@@ -84,6 +84,7 @@ export const AuthContextProvider = ({ children }) => {
       userLastName: tokenDecoded.apellido,
     };
     authInfo.userRole = await getUserRole(authInfo.userEmail, token);
+    console.log(authInfo.userRole);
     setAuth(authInfo);
 
     setIsLoading(false);
