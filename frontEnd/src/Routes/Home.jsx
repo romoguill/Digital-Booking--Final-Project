997a532d191d250491f7a0e2fc0fa71a381b9cf8
Manu Zarraga <manuzarraga@hotmail.com>
2023-03-31 13:46:12 -0300

    final reservations commit

--- frontEnd/src/Routes/Home.jsx
+++ frontEnd/src/Routes/Home.jsx
@@ -2,7 +2,6 @@ import HomeSearch from '../Components/Body/HomeSearch';
 import CarrouselCategories from '../Components/Body/CarrouselCategories';
 import GridRentals from '../Components/Body/GridRentals';
 import MenuDrawerMobile from '../Components/Header/MenuDrawerMobile/MenuDrawerMobile';
-import MyReservations from './MyReservations';
 
 function Home({ menuDrawerVisible, setMenuDrawerVisible }) {
   return (
